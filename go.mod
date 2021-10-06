a27d4c799eb016dd5db8b59c4f28e634988cf770
AntonyIS <antony@ariyafinergy.com>
2021-10-06 12:39:21 +0300

    bug fix in getitems handler

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/AntonyIS/Golang-microservice-1/database v0.0.0-20211006091456-2af88852a6c2
+	github.com/AntonyIS/Golang-microservice-1/handlers v0.0.0-20211006093455-f92653059adc
 	github.com/gin-gonic/gin v1.7.4
-	github.com/jinzhu/gorm v1.9.16
 )
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,13 +3,19 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/AntonyIS/Golang-microservice-1/database"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
+var db *gorm.DB
+
 // Return slice of all items available in the system
-func GetItems(c *gin.Context, db *gorm.DB) {
-	var items []Item
+func GetItems(c *gin.Context) {
+
+	var items []database.Item
 	db.Find(&items)
-	return items
+	c.IndentedJSON(http.StatusOK, items)
 }
